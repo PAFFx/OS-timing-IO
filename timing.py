4dbc9689b7cac4e8719414099d6aebec00c69290
PAFFx <jj2jom@gmail.com>
2023-08-10 17:55:11 +0700

    better spacing

--- timing.py
+++ timing.py
@@ -4,7 +4,7 @@ def IO_timer(epochs: int):
     f = open("timer_demo.txt", "w")
     time_start = time.time()
     for i in range(0,epochs):
-        f.write(f"IO, #%d" %i)
+        f.write(f"IO, #{i}")
     time_end = time.time()
     time_dif = time_end - time_start
     print("execution time for ",epochs," I/Os : ","{:e}".format(time_dif)," ms")
@@ -18,17 +18,16 @@ def max_list_timer(epochs: int):
     time_dif = time_end - time_start
     print("execution time for finding ",epochs," 2x2 python's list max value : ","{:e}".format(time_dif)," ms")
 
+def compare_time(epochs: int):
+    print("-------epochs-"+str(epochs)+"-------")
+    IO_timer(epochs)
+    max_list_timer(epochs)
+    print("\n")
 
 if __name__ == "__main__":
-    IO_timer(10)
-    max_list_timer(10)
-    IO_timer(100)
-    max_list_timer(100)
-    IO_timer(1000)
-    max_list_timer(1000)
-    IO_timer(10000)
-    max_list_timer(10000)
-    IO_timer(100000)
-    max_list_timer(100000)
-    IO_timer(1000000)
-    max_list_timer(1000000)
+    compare_time(10)
+    compare_time(100)
+    compare_time(1000)
+    compare_time(10000)
+    compare_time(100000)
+    compare_time(1000000)
