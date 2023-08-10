89b41f8382257257011bbc36cb3be100be554712
PAFFx <jj2jom@gmail.com>
2023-08-10 17:07:20 +0700

    use timer_demo.txt to write file instead of print

--- timing.py
+++ timing.py
@@ -1,21 +1,28 @@
 import time
 
-def IO_timer(total_IO:int):
+def IO_timer(total_IO: int):
+    f = open("timer_demo.txt", "w")
     time_start = time.time()
     for i in range(0,total_IO):
-        print("IO #",i)
+        f.write(f"IO, #%d" %i)
     time_end = time.time()
     print("execution time for ",total_IO,"I/O : ",time_end-time_start," ms")
 
 
 if __name__ == "__main__":
     IO_timer(0)
-    IO_timer(1)
-    IO_timer(2)
-    IO_timer(3)
-    IO_timer(4)
-    IO_timer(5)
-    IO_timer(6)
-    IO_timer(7)
+    IO_timer(10)
+    IO_timer(100)
+    IO_timer(1000)
+    IO_timer(int(1e3))
+    IO_timer(int(1e4))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
+    IO_timer(int(1e3))
     
     
