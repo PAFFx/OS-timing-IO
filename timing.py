4de4bd8e44d41677e5910ac9c6ce056c0f62bb7f
PAFFx <jj2jom@gmail.com>
2023-08-10 17:50:56 +0700

    use 2x2 list max value

--- timing.py
+++ timing.py
@@ -1,5 +1,4 @@
 import time
-import numpy as np
 
 def IO_timer(epochs: int):
     f = open("timer_demo.txt", "w")
@@ -7,22 +6,29 @@ def IO_timer(epochs: int):
     for i in range(0,epochs):
         f.write(f"IO, #%d" %i)
     time_end = time.time()
-    print("execution time for ",epochs," I/Os : ",time_end-time_start," ms")
+    time_dif = time_end - time_start
+    print("execution time for ",epochs," I/Os : ","{:e}".format(time_dif)," ms")
 
-def matrix_mul_timer(epochs: int):
-    a = np.ndarray(8);
-    a.reshape(2,2,2)
+def max_list_timer(epochs: int):
+    a = [[1,2],[3,4]]
     time_start = time.time()
     for _ in range(0,epochs):
-        a.max()
+        max(a)
     time_end = time.time()
-    print("execution time for finding ",epochs," 2x2x2 numpy tensor max value : ",time_end-time_start," ms")
+    time_dif = time_end - time_start
+    print("execution time for finding ",epochs," 2x2 python's list max value : ","{:e}".format(time_dif)," ms")
 
 
 if __name__ == "__main__":
     IO_timer(10)
-    matrix_mul_timer(10)
+    max_list_timer(10)
     IO_timer(100)
-    matrix_mul_timer(100)
+    max_list_timer(100)
     IO_timer(1000)
-    matrix_mul_timer(1000)
+    max_list_timer(1000)
+    IO_timer(10000)
+    max_list_timer(10000)
+    IO_timer(100000)
+    max_list_timer(100000)
+    IO_timer(1000000)
+    max_list_timer(1000000)
